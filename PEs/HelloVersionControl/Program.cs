a3605ae62f4d33558fcfe27023d28f5a85962e2b
Reilly Lawson <rbl2991@rit.edu>
2022-09-23 13:55:10 -0400

    Three new lines for commit number 2

--- PEs/HelloVersionControl/Program.cs
+++ PEs/HelloVersionControl/Program.cs
@@ -7,6 +7,10 @@
             Console.WriteLine("Hello, World 1");
             Console.WriteLine("Hello, World 2");
             Console.WriteLine("Hello, World 3");
+
+            Console.WriteLine("Hello, World 4!");
+            Console.WriteLine("Hello, World 5!");
+            Console.WriteLine("Hello, World 6!");
         }
     }
 }
