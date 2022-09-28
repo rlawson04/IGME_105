2fc818a55f8df2ec57086356b3cfa7e4b55c88ca
Reilly Lawson <rbl2991@rit.edu>
2022-09-28 13:48:31 -0400

    info at top

---
+++ PEs/PE12/Program.cs
@@ -0,0 +1,13 @@
+﻿//Reilly Lawson
+//9/28/2022
+//Loops and GitHub practice exercise
+namespace PE12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
