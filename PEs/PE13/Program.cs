2153fa0d2dc9b55234cf2dae4878c8c4640a8d87
Reilly Lawson <rbl2991@rit.edu>
2022-09-30 12:49:18 -0400

    initial commit

---
+++ PEs/PE13/Program.cs
@@ -0,0 +1,14 @@
+﻿//Reilly Lawson
+//9/30/22
+//Loops practice
+
+namespace PE13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
