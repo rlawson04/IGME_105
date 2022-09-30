27575018e5f0ff6a37e511eabfeae8c6dba44b5b
Reilly Lawson <rbl2991@rit.edu>
2022-09-30 12:47:28 -0400

    Final nested loops demo

--- Demos/Nested Loop Demos/Program.cs
+++ Demos/Nested Loop Demos/Program.cs
@@ -8,12 +8,25 @@ namespace Nested_Loop_Demos
     {
         static void Main(string[] args)
         {
+            //Inter loop runs all the way and then the outer loop runs once.
             for (int x = 0; x < 10; x++)
             {
                 for (int y = 0; y < 10; y++)
-                    Console.Write(".");
+                { 
+                Console.Write(".");
+                }
+
+                Console.Write("\n");
             }
-            Console.Write("\n");
+
+            for(int r = 0; r < 15; r++)
+            {
+                for (int c = 0; c < 15; c++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
