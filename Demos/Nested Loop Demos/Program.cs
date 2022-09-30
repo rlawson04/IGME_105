f115de9a18e583ebc564a73925f1b67582243678
Reilly Lawson <rbl2991@rit.edu>
2022-09-30 12:40:05 -0400

    initial commit

---
+++ Demos/Nested Loop Demos/Program.cs
@@ -0,0 +1,19 @@
+﻿//Reilly Lawson
+//9/30
+//Nested loops
+
+namespace Nested_Loop_Demos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                    Console.Write(".");
+            }
+            Console.Write("\n");
+        }
+    }
+}
