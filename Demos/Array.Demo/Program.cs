f4e8d63886d499bba1c53ffa9030697d3f4a6676
Reilly Lawson <rbl2991@rit.edu>
2022-10-12 13:27:21 -0400

    Initial

---
+++ Demos/Array.Demo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Array.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phoneNumbers;
+            phoneNumbers = new string[4]; 
+        }
+    }
+}
