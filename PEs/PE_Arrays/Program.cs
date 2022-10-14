0cb423a25d29e713715d0bc597c285c2d2a84389
Reilly Lawson <rbl2991@rit.edu>
2022-10-13 20:39:13 -0400

    Initial commit

---
+++ PEs/PE_Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PE_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the character name collector!");
+        }
+    }
+}
