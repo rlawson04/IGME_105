8bbb015a856a5839029e4eaebdc7d613aea10330
Reilly Lawson <rbl2991@rit.edu>
2022-10-14 13:07:33 -0400

    Initial Commit

---
+++ Demos/Array Methods/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Array_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create some literal arrays
+            int[] numbers = { 2, 4, 6, 8, 10 };
+            string[] words = { "Hi", "ok", "cool" };
+
+            Console.WriteLine();
+        }
+    }
+}
