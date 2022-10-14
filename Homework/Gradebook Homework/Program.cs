ccd4f555d0ce5a7f6a146f744236ff08881f3186
Reilly Lawson <rbl2991@rit.edu>
2022-10-14 13:48:34 -0400

    Initial commit

---
+++ Homework/Gradebook Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Gradebook_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables to be used throughout the program
+            int numberOfAssignments = 0;
+            string userIput = "";
+
+            //Get number of assignments from user to help initialize the arrays
+            Console.WriteLine("How many exams are you saving");
+
+
+            //Arrays for the values and names of the exams
+            string[] examNames = new string[numberOfAssignments];
+            double[] examValues = new double[numberOfAssignments];
+
+        }
+    }
+}
