0613306b297a1099ec15527520fbd6dafd9d024d
Reilly Lawson <rbl2991@rit.edu>
2022-10-17 13:48:00 -0400

    commit

---
+++ Demos/Value And Reference Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Value_And_Reference_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Two arrays with values
+            //a is a reference for b
+            int[] a = { 5, 10, 15 };
+            int[] b = a;
+
+            //Changing a element 0 also changes b element 0
+            a[0] = 2000;
+
+            //Value types
+            int c = 5;
+            int d = c;
+            c = 200;
+            //Value c becomes 200 but d stays as 5
+            //since it makes a copy at a certain point (where c == 5)
+            //even though the original changes, the copy doesn't
+        }
+    }
+}
