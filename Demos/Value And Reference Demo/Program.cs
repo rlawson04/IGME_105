1b1ae133f26c88a3d4c9212134df52070a722f53
Reilly Lawson <rbl2991@rit.edu>
2022-10-19 12:58:54 -0400

    1000000 arrays

--- Demos/Value And Reference Demo/Program.cs
+++ Demos/Value And Reference Demo/Program.cs
@@ -19,6 +19,20 @@
             //Value c becomes 200 but d stays as 5
             //since it makes a copy at a certain point (where c == 5)
             //even though the original changes, the copy doesn't
+
+
+            Console.WriteLine("Start");
+            //Create 100,000,000 new arrays
+            for (int i = 0; i < 100_000_000; i++)
+            {
+                // Declare and initialize a new array, size 2, of ints
+                int[] newArray = new int[2];
+
+                // Insert data of 1 and 2 at index 0 and 1
+                newArray[0] = 1;
+                newArray[1] = 2;
+            }
+            Console.WriteLine("End");
         }
     }
 }
