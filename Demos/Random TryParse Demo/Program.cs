7d02e980b19d92d7961eab744665a0b6bf93dba0
Reilly Lawson <rbl2991@rit.edu>
2022-10-21 12:09:38 -0400

    Initial Commit

---
+++ Demos/Random TryParse Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Random_TryParse_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Try Parse
+            Console.WriteLine("Enter a double: ");
+            double result;
+            bool success = double.TryParse(Console.ReadLine(), out result);
+
+            Console.WriteLine("Result is " + result);
+            Console.WriteLine("Success is " + success);
+
+            while(!success)
+            {
+                //reprompt
+                Console.WriteLine("Enter a double: ");
+                success = double.TryParse(Console.ReadLine(), out result);
+                Console.WriteLine("Result is " + result);
+                Console.WriteLine("Success is " + success);
+            }
+
+        }
+    }
+}
---
+++ Exams/RBL_GDAPS1_Midterm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RBL_GDAPS1_Midterm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
