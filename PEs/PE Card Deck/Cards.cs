788eb2063e0593abbdadeb631bb4c13de6813181
Reilly Lawson <rbl2991@rit.edu>
2022-10-28 13:50:37 -0400

    Fix Cards Constructor

---
+++ PEs/PE Card Deck/Cards.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Card_Deck
+{
+    internal class Cards
+    {
+        //Fields
+        private int value;
+        private string suit;
+
+        //Constructors
+
+        /// <summary>
+        /// Fix this parameterized constructor
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="suit"></param>
+        /// <returns></returns>
+        Public Cards (int value, string suit)
+        {
+            this.value = value;
+            this.suit = suit;
+        }
+
+        //Methods
+    }
+}
---
+++ PEs/PE Card Deck/Deck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Card_Deck
+{
+    internal class Deck
+    {
+        //Fields
+
+
+        //Constructors
+
+
+        //Methods
+     
+    }
+}
