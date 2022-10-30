a2e44e12e49ccedcb2b1832a9e6ea84ff0867db1
Reilly Lawson <rbl2991@rit.edu>
2022-10-30 18:49:45 -0400

    Fixed it

--- PEs/Homework 5 Monopoly/Dice.cs
+++ PEs/Homework 5 Monopoly/Dice.cs
@@ -15,19 +15,32 @@ namespace Homework_5_Monopoly
         
 
         //Methods
+        
+        /// <summary>
+        /// Generates a random number between 1-6 (inclusive)
+        /// </summary>
+        /// <returns></returns>
         public int RollDie()
         { 
             randomNum = new Random();
             return randomNum.Next(1, 7); 
         }
 
+        /// <summary>
+        /// Generates the sum of how many dice are rolled
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
         public int RollDice(int amount)
         {
-            // Add the RollDie method twice to simulate two dice being rolled
-            amount = RollDie();
-            amount += RollDie();
+            int sum = 0;
 
-            return amount;
+           for (int i = 0; i < amount; i++)
+            {
+                sum += RollDie();
+            }
+
+            return sum;
         }
 
     }//End of internal class
--- PEs/Homework 5 Monopoly/Program.cs
+++ PEs/Homework 5 Monopoly/Program.cs
@@ -1,22 +1,36 @@
-﻿namespace Homework_5_Monopoly
+﻿using System.Security.Cryptography;
+
+namespace Homework_5_Monopoly
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //Variables to be used throughout the program
-            int sumOfDice = 0;
+            double sumOfDice = 0.0;
 
             //Initialize dice class
             Dice dice = new Dice();
 
             for (int i = 0; i < 100; i++)
             {
-                
-                Console.Write($"{dice.RollDie()} ");
-                
+                int value = dice.RollDie();
+                sumOfDice += value;
+                Console.Write($"{value} ");
             }
+            Console.WriteLine();
             Console.WriteLine($"Average roll for a single die: {sumOfDice/100}");
-        }
-    }
+
+            for (int i = 0; i < 100; i++)
+            {
+                int value = dice.RollDice(2);
+                sumOfDice += value;
+                Console.Write($"{value} ");
+
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Average roll for a single die: {sumOfDice / 100}");
+
+        }// End of Main
+    }//End of Program
 }
