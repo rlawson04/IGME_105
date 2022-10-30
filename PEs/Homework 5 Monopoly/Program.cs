b63ebea385d32cebca7c3204b2c3ddd99cd7dbe4
Reilly Lawson <rbl2991@rit.edu>
2022-10-30 15:27:43 -0400

    Initial Commit

---
+++ PEs/Homework 5 Monopoly/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_5_Monopoly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
