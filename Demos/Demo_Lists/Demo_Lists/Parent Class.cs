f45828fc774fbb7a2f73733a4e0a07f719ff0685
Reilly Lawson <rbl2991@rit.edu>
2022-11-09 13:49:34 -0500

    Final in class version

---
+++ Demos/Demo_Lists/Demo_Lists/Parent Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Lists
+{
+    internal class Parent_Class
+    {
+        protected string word;
+        protected int number;
+
+        public string Word
+        {
+            get { return word; }    
+        }
+
+        public Parent_Class()
+        {
+            number = 10;
+            word = "peanut";
+        }
+    }
+}
--- Demos/Demo_Lists/Demo_Lists/Program.cs
+++ Demos/Demo_Lists/Demo_Lists/Program.cs
@@ -9,20 +9,70 @@
             List<string> myList2 = new List<string>();
             List<bool> myList3 = new List<bool>();
 
+            // Inspect using properties
+            Console.WriteLine("List count " + myList.Count);
+            Console.WriteLine("List capacity " + myList.Capacity);
+
             // Adding values to the list
             myList.Add(3.14);
+
+            // Inspect using properties
+            Console.WriteLine("List count " + myList.Count);
+            Console.WriteLine("List capacity " + myList.Capacity);
+
             myList.Add(7.27);
+
+            // Inspect using properties
+            Console.WriteLine("List count " + myList.Count);
+            Console.WriteLine("List capacity " + myList.Capacity);
+            
             myList.Add(2.0);
+
+            // Inspect using properties
+            Console.WriteLine("List count " + myList.Count);
+            Console.WriteLine("List capacity " + myList.Capacity);
+
             myList.Add(1);
+
+            // Inspect using properties
+            Console.WriteLine("List count " + myList.Count);
+            Console.WriteLine("List capacity " + myList.Capacity);
+
             myList.Add(2);
-            myList.Add(3);  
+
+            // Inspect using properties
+            Console.WriteLine("List count " + myList.Count);
+            Console.WriteLine("List capacity " + myList.Capacity);
+
+            myList.Add(3);
+
+            // Inspect using properties
+            Console.WriteLine("List count " + myList.Count);
+            Console.WriteLine("List capacity " + myList.Capacity);
 
             myList[5] = 1.2345;
+
+            // Inspect using properties
+            Console.WriteLine("List count " + myList.Count);
+            Console.WriteLine("List capacity " + myList.Capacity);
 
             // .Count is the list equivalent of .Length
             Console.WriteLine("The size of this list is " + myList.Count);
 
             myList2.Add("phone");
+            
+            // Foreach loop that prints the list
+            foreach(double number in myList)
+            {
+                Console.WriteLine(number);
+            }
+
+            // For loop that prints the list
+            for (int i = 0; i < myList.Count; i+=2)
+            {
+                Console.WriteLine(myList[i]);
+                myList[i] = 999;
+            }
         }
     }
 }
