7128b7cd6c0d126c050abc67182f31c9fce3218a
Reilly Lawson <rbl2991@rit.edu>
2022-11-21 13:14:10 -0500

    final in class version

--- Demos/Demo_TextFileIO_DoubleReadline/Program.cs
+++ Demos/Demo_TextFileIO_DoubleReadline/Program.cs
@@ -10,6 +10,26 @@
 
                 // Read in all lines of text from the file
                 string lineOfText = "";
+
+                while((lineOfText = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(lineOfText);
+
+                    // Do something meaningful with the data!
+                    string[] splitData = lineOfText.Split('|');
+                    // What can we do here?  How do we know if its a Monster, Zombie or Vampire?
+                    // How can we use that data to instantiate an appropriate object?
+                    
+                if (splitData[0] == "Monster")
+                    {
+                        Monster guy = new Monster
+                            (splitData[1],              // Name
+                            int.Parse(splitData[2]),    // Age
+                            int.Parse(splitData[3]));   // Const
+                    }
+                }
+
+                /*
                 while (lineOfText != null)
                 {
                     // With the ReadLine inside the body of this while loop, 
@@ -28,6 +48,7 @@
                         // How can we use that data to instantiate an appropriate object?
                     }
                 }
+                */
 
                 // Close the stream
                 reader.Close();
