4ad9ed3ff2c4ea1abe44cd67613b68a9b4b43faf
Reilly Lawson <rbl2991@rit.edu>
2022-11-21 12:38:03 -0500

    Initial Commit

---
+++ Demos/Demo_TextFileIO_DoubleReadline/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Demo_TextFileIO_DoubleReadline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader("../../../Monsters.txt");
+
+                // Read in all lines of text from the file
+                string lineOfText = "";
+                while (lineOfText != null)
+                {
+                    // With the ReadLine inside the body of this while loop, 
+                    //   we must check for null.
+                    // Null is returned when the stream runs out of data to read.
+                    lineOfText = reader.ReadLine();
+
+                    // Ensure that the stream returned actual data from the file...
+                    if (lineOfText != null)
+                    {
+                        Console.WriteLine(lineOfText);
+
+                        // Do something meaningful with the data!
+                        //string[] splitData = lineOfText.Split('|');
+                        // What can we do here?  How do we know if its a Monster, Zombie or Vampire?
+                        // How can we use that data to instantiate an appropriate object?
+                    }
+                }
+
+                // Close the stream
+                reader.Close();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("File read error: " + error.Message);
+            }
+        }
+    }
+}
