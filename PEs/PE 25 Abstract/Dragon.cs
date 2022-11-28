dd0e04c7712a48f31cb592f838fa077e929f01f1
Reilly Lawson <rbl2991@rit.edu>
2022-11-28 13:50:05 -0500

    Work on Methods for Dragon class

---
+++ PEs/PE 25 Abstract/Dragon.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_25_Abstract
+{
+    internal class Dragon : Monster
+    {
+        //-------------------------------------------------
+        // Fields
+        //-------------------------------------------------
+
+        private string resistanceType;
+
+        //-------------------------------------------------
+        // Properties
+        //-------------------------------------------------
+
+
+
+        //-------------------------------------------------
+        // Constructors
+        //-------------------------------------------------
+
+        /// <summary>
+        /// Parameterized Constructor
+        /// </summary>
+        /// <param name="name"> takes a string </param>
+        /// <param name="health"> takes an int </param>
+        /// <param name="attackType"> takes a string </param>
+        /// <param name="resistanceType"> takes a string </param>
+        public Dragon (string name, int health, string attackType, string resistanceType) 
+            : base (name, health, attackType)
+        {
+            this.resistanceType = resistanceType;
+        }
+
+        //-------------------------------------------------
+        // Methods
+        //-------------------------------------------------
+
+
+    }
+}
--- PEs/PE 25 Abstract/Monster.cs
+++ PEs/PE 25 Abstract/Monster.cs
@@ -48,9 +48,73 @@ namespace PE_25_Abstract
         // Constructors
         //-------------------------------------------------
 
+        /// <summary>
+        /// Parameterized constructor 
+        /// </summary>
+        /// <param name="name"> takes a string </param>
+        /// <param name="health"> takes an int </param>
+        /// <param name="attackType"> takes a string </param>
+        
+        public Monster
+        (
+            string name,
+            int health,
+            string attackType
+        )
+        {
+            this.name = name;
+            this.health = health;
+            this.attackType = attackType;
+            randomGenerator = new Random();
+
+        }
 
         //-------------------------------------------------
         // Methods
         //-------------------------------------------------
+
+        /// <summary>
+        /// Attacks a target
+        /// </summary>
+        /// <param name="monster"> takes a monster object </param>
+        public abstract void Attack(Monster monster);
+
+        /// <summary>
+        /// Takes damage of a specific amount and type
+        /// </summary>
+        /// <param name="damage"> takes an int </param>
+        /// <param name="attackType"> takes a string </param>
+        public abstract void TakeDamage(int damage, string attackType);
+
+        /// <summary>
+        /// An overriden ToString method that changes based on the attackType of the monster
+        /// </summary>
+        /// <returns> Unique String </returns>
+        public override string ToString()
+        {
+            switch (attackType)
+            {
+                case "fire":
+                return $"{name} has {health} health and breathes fire on its enemies";
+                    break;
+
+                case "ice":
+                    return $"{name} has {health} health and freezes its enemies";
+                    break;
+
+                case "earth":
+                    return $"{name} has {health} health and creates powerful earthquakes";
+                    break;
+
+                case "lightning":
+                    return $"{name} has {health} health and summons lightning strikes";
+                    break;
+
+                default:
+                    return name;
+            }
+        } 
     }
+
+    
 }
