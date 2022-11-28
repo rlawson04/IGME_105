540cf43ca9df9285c75ed3f4f031883e6c7d9000
Reilly Lawson <rbl2991@rit.edu>
2022-11-28 13:06:21 -0500

    Initial Commit

---
+++ PEs/PE 25 Abstract/Program.cs
@@ -0,0 +1,14 @@
+﻿// Reilly Lawson
+// 11/28/2022
+// Using Abstract keyword
+
+namespace PE_25_Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
