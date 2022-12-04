f1f11c1fe0e4a3012acd2386542c988f8cdd0859
Reilly Lawson <rbl2991@rit.edu>
2022-12-04 13:54:42 -0500

    Initial Commit

---
+++ Homework/HW7_BattleDamage_STARTER/HW7_BattleDamage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+/// <summary>
+/// Empty starter project with the shipstats.csv data file for HW 7
+/// As configured, the relative path for shipstats.csv in this project is
+///         "../../../shipstats.csv"
+/// https://docs.google.com/document/d/1PZlqdP3MJlPMuIFZiztK-_R0iUy654XPnbFu85Xcuvo/edit?usp=sharing
+/// </summary>
+namespace HW7_BattleDamage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
