360982ee8ce9558a61009082a5d10a27a78c1dc0
Reilly Lawson <rbl2991@rit.edu>
2022-12-04 20:06:27 -0500

    Working on Load Data Method

---
+++ Homework/HW7_BattleDamage_STARTER/HW7_BattleDamage/Ship.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW7_BattleDamage
+{
+    /// <summary>
+    /// Ship child class that inherits everything from the ShipData base class
+    /// </summary>
+    internal class Ship : ShipData
+    {
+        // --------------------------------------------------------
+        // Fields
+        // --------------------------------------------------------
+
+        // --------------------------------------------------------
+        // Properties
+        // --------------------------------------------------------
+
+        // --------------------------------------------------------
+        // Constructors
+        // --------------------------------------------------------
+
+        // --------------------------------------------------------
+        // Methods
+        // --------------------------------------------------------
+    }
+}
---
+++ Homework/HW7_BattleDamage_STARTER/HW7_BattleDamage/ShipData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW7_BattleDamage
+{
+    /// <summary>
+    /// Base class for the ship child class that will inherit basic information for all ships
+    /// </summary>
+    internal class ShipData
+    {
+        // --------------------------------------------------------
+        // Fields
+        // --------------------------------------------------------
+
+        protected string[,] data;
+
+        // --------------------------------------------------------
+        // Properties
+        // --------------------------------------------------------
+
+        // --------------------------------------------------------
+        // Constructors
+        // --------------------------------------------------------
+
+
+
+        // --------------------------------------------------------
+        // Methods
+        // --------------------------------------------------------
+
+        public static void LoadData()
+        {
+            
+            try 
+            {
+                // Opens the file as a stream reader
+                StreamReader reader = new StreamReader("../../../shipstats.csv");
+                
+                // Variable to keep track of the individual lines
+                string lineOfText = " ";
+
+                // Reads the first line of the file
+                lineOfText = reader.ReadLine();
+
+                // Turns the first line of the file into an int
+                int numberOfRows = int.Parse(lineOfText);
+               
+                string[,] data = new string[numberOfRows, 6];
+
+                while (lineOfText != null)
+                {
+                    lineOfText.Split(',');
+                }
+                
+                // Closes the file when done
+                reader.Close();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+            
+        
+    }
+}
