81b805eafe3ddb2071b014c0692819f8686abf81
Reilly Lawson <rbl2991@rit.edu>
2022-12-06 21:23:14 -0500

    Final Version

---
+++ Demos/Demo_MultidimensionalArrays/Program.cs
@@ -0,0 +1,44 @@
+﻿
+// Erin Cascioli
+// 11/30
+// Demo: Multidimensional arrays
+
+namespace Demo_MultidimensionalArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a multi-dimensional (2D array) array where the rows contain a name and birthday
+            // Name     Birthday
+            string[,] birthdays = new string[5, 2];
+            int numRows = birthdays.GetLength(0);
+            int numCols = birthdays.GetLength(1);
+
+            // Add data to the 2D array
+            birthdays[0,0] = "Andrew";              // Name
+            birthdays[0,1] = "December 23";         // Date
+            birthdays[1, 0] = "Emmett";             // Name
+            birthdays[1, 1] = "December 16";        // Date
+            birthdays[2, 0] = "Reilly";             // Name
+            birthdays[2, 1] = "April 4";            // Date
+            birthdays[3, 0] = "Jalen";              // Name
+            birthdays[3, 1] = "October 29";         // Date
+            birthdays[4, 0] = "Ayden";              // Name
+            birthdays[4, 1] = "December 20";        // Date
+
+            // Iterate through the array, printing each student's name and their birthday
+            for(int row = 0; row < numRows; row++)
+            {
+                for(int col = 0; col < numCols; col++)
+                {
+                    Console.Write(birthdays[row, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            // Can still get index out of range!
+            //Console.WriteLine(birthdays[5, 5]);
+        }
+    }
+}
--- Homework/HW7_BattleDamage_STARTER/HW7_BattleDamage/ShipData.cs
+++ Homework/HW7_BattleDamage_STARTER/HW7_BattleDamage/ShipData.cs
@@ -21,17 +21,32 @@ namespace HW7_BattleDamage
         // Properties
         // --------------------------------------------------------
 
-        // --------------------------------------------------------
-        // Constructors
-        // --------------------------------------------------------
+        /// <summary>
+        /// Get only property that returns 0 or the number of rows if data has been loaded
+        /// </summary>
+        public int Count
+        {
+            get 
+            { 
+                // If no data has been loaded then return 0
+                if (data.GetLength(0) == 0)
+                {
+                    return 0;
+                }
 
-
+                // Return the number of rows if data has been loaded 
+                return data.GetLength(0);
+            }
+        }
 
         // --------------------------------------------------------
         // Methods
         // --------------------------------------------------------
 
-        public static void LoadData()
+        /// <summary>
+        /// Reads data from the file and fills a 2d Array with information
+        /// </summary>
+        public void LoadData()
         {
             
             try 
@@ -39,31 +54,112 @@ namespace HW7_BattleDamage
                 // Opens the file as a stream reader
                 StreamReader reader = new StreamReader("../../../shipstats.csv");
                 
-                // Variable to keep track of the individual lines
-                string lineOfText = " ";
-
-                // Reads the first line of the file
-                lineOfText = reader.ReadLine();
+                // Variable to keep track of the individual lines and read the first line 
+                string lineOfText = reader.ReadLine();
 
                 // Turns the first line of the file into an int
                 int numberOfRows = int.Parse(lineOfText);
                
-                string[,] data = new string[numberOfRows, 6];
+                // Initialize data field and create the 2d array
+                data = new string[numberOfRows, 6];
+                
+                // Reads second line which is not saved
+                lineOfText = reader.ReadLine();
 
+                // Integer to keep track of what row of the 2d array
+                // is currently being filled
+                int row = 0;
+
+                // While the reader doesn't hit a null statement, keep reading
                 while (lineOfText != null)
                 {
-                    lineOfText.Split(',');
+                    lineOfText = reader.ReadLine();
+
+                    // Breaks the loop if it picks up that lineOfText is null after
+                    // it reads the next line
+                    if (lineOfText == null)
+                    {
+                        break;
+                    }
+
+                    // Splits the data into an array
+                    string[] arrayOfLines = lineOfText.Split(',');
+
+                    // For loop that adds the split data into the 2d array
+                    for (int col = 0; col < data.GetLength(1); col++)
+                    {
+                        data[row, col] = arrayOfLines[col];
+                    }
+                    
+                    // Updates the row so that the 2d array can properly fill
+                    row++;
                 }
                 
                 // Closes the file when done
                 reader.Close();
+
+                // Prints how many rows were read
+                Console.WriteLine($"{data.GetLength(0)} records read from shipstats.csv");
             }
             catch (Exception error)
             {
                 Console.WriteLine(error.Message);
             }
+        }// End of Load Data Method
+
+        /// <summary>
+        /// Copies the data of a specific row from the 2D array that was loaded
+        /// </summary>
+        /// <param name="row"> takes an int </param>
+        /// <returns> A copy of a specific row of data </returns>
+        public string[] GetShipData(int row)
+        {
+            string[] copyOfData = new string[6];
+            try {    
+                
+                // Iterate through each index in the given row
+                // and add it to the new array
+                for(int i = 0; i < data.GetLength(1); i++)
+                {
+                    copyOfData[i] = data[row, i];
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"The specified row {row} is out" +
+                    $" of the range of 0 to {data.GetLength(1)-1}");
+            }
+
+            // Return the new array with the row information
+            return copyOfData;
+        } // End of get ship data method
+
+        /// <summary>
+        /// Overrides the to string method to return the 2D array in a nice format
+        /// </summary>
+        /// <returns> returns a table with all the information of each ship </returns>
+        public override string ToString()
+        {
+            // Variables to keep track of colums and rows, and the string to be
+            // returned at the end of the method
+            int numRows = data.GetLength(0);
+            int numCols = data.GetLength(1);
+            string arrayInfo = $"Faction Name\tShip Name\tWeapon\tAgility\tHull\tShields\n";
+
+            // Iterate through the array, each ship and their stats
+            for (int row = 0; row < numRows; row++)
+            {
+                for (int col = 0; col < numCols; col++)
+                {
+                   // Add the information to the string 
+                   arrayInfo += (data[row, col] + "\t");
+                }
+                // Make a new line for the next row in the array
+                arrayInfo += "\n";
+            }
+
+            // Return the final string
+            return arrayInfo;
         }
-            
-        
     }
 }
