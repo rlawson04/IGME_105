e5014ecdcc7e49d51fbb963f385b73d93793fac1
Reilly Lawson <rbl2991@rit.edu>
2022-12-08 13:42:03 -0500

    Initial Commit

---
+++ Exams/GDAPS1_FinalPractical_STARTER/Item.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace GDAPS1_FinalPractical
+{
+    /// <summary>
+    /// The Item class provides a foundation for all other classes
+    /// you'll create for this practical.
+    ///
+    /// DO NOT MODIFY **ANY** CODE IN THIS CLASS
+    /// </summary>
+    abstract class Item
+    {
+        /// <summary>
+        /// All items have a name
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        /// The name can always be read via a property
+        /// </summary>
+        public string Name { get { return name; } }
+
+        /// <summary>
+        /// All items also have a weight, but Item doesn't
+        /// know how to get it
+        /// </summary>
+        abstract public double Weight { get; }
+
+        /// <summary>
+        /// All items must be created with at least a name
+        /// </summary>
+        /// <param name="name">The name to give to this item</param>
+        public Item(string name)
+        {
+            this.name = name.ToUpper();
+        }
+
+        /// <summary>
+        /// All items can proide their name and weight as a 
+        /// well formatted string.
+        /// </summary>
+        /// <returns>A string containing the item's name and weight</returns>
+        public override string ToString()
+        {
+            return String.Format("{0}: {1:F2} lbs",
+                name,
+                Weight);
+        }
+    }
+}
---
+++ Exams/GDAPS1_FinalPractical_STARTER/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace GDAPS1_FinalPractical
+{
+    class Program
+    {
+
+        /// <summary>
+        /// An outline of Main() has been written for you with some test code and
+        /// prompts for questions to answer
+        /// 
+        /// DO NOT MODIFY **except** where clearly marked with: 
+        ///    - TODO: Enter your response in a comment here
+        ///                 -or-
+        ///    - TODO: UNCOMMENT the following
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // Note: Yes, normally all variables used within the method should be declared
+            // at the top of the method. This is not done in Main() to allow isolation of code
+            // that won't compile until you complete each activity.
+
+            /********************************************/
+            /* ACTIVITY 1: Setup a basic inventory      */
+            /********************************************/
+
+            /**
+             * Item ToString explanation
+             *  - Using Weight in the Item class implementation of ToString
+             *      results in the item's weight being included
+             *      in the result of this method.
+             *  - Why will this work even though the Item class doesn't implement 
+             *      this property or have a "weight" field itself?
+             *      
+             *  NOTE: You do NOT need to write any code for this question. You are
+             *  ONLY writing comments below to explain how ToString works in this
+             *  scenario.
+             */
+            // TODO: Enter your response about Item ToString in a comment here
+
+            // TODO: UNCOMMENT the following to test Activity 1
+/*            Console.WriteLine("\n\n** Activity 1 **");
+            Inventory myItems = new Inventory();
+            myItems.AddItem(new Weapon("Sharp Stick", 5, 0.05));
+            myItems.PrintSummary();
+*/
+
+            /********************************************/
+            /* ACTIVITY 2: Add support for foods        */
+            /********************************************/
+
+            // TODO: UNCOMMENT the following to test Activity 3
+/*            Console.WriteLine("\n\n** Activity 2 **");
+            Food testFood = new Food("Apple", 2, 0.25);
+            testFood.Eat();
+            testFood.Eat();
+            testFood.Eat();
+            myItems.AddItem(testFood);
+            myItems.AddItem(new Food("Pumpkin Pie", 8, 0.1));
+            myItems.AddItem(new Food("Cookies", 25, 0.03));
+            myItems.PrintSummary();
+*/
+
+            /********************************************/
+            /* ACTIVITY 3: Load more items from a file  */
+            /********************************************/
+
+            // TODO: UNCOMMENT the following to test Activity 3
+/*            Console.WriteLine("\n\n** Activity 3 **");
+            myItems.LoadItems("../../../BAD.txt");
+            myItems.LoadItems("../../../items.txt");
+            myItems.LoadItems("../../../items.txt");
+            myItems.PrintSummary();
+*/
+            /********************************************/
+            /* ACTIVITY 4: Lighten the load             */
+            /********************************************/
+
+            // TODO: UNCOMMENT the following to test Activity 4
+/*            Console.WriteLine("\n\n** Activity 4 **");
+            myItems.LightenLoad();
+            myItems.PrintSummary();
+*/
+        }
+
+    }
+}
